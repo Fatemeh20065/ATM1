6b1ebfe05723e0801533c19bf88cf180513d7bde
fatemeh mardani <fatemehmardani138520065@gmail.com>
2025-10-31 22:33:32 +0330

    ATM project1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+     int mony , x = 10000 , y = 50000 ;
+            Console.WriteLine("how much money do you want ?");
+            mony = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= x; i++)
+            {
+                for (int j = 0; j <= y; j++)
+                {
+                    if (i * x + j * y == mony)
+                    {
+                        Console.WriteLine("number money 10000 : {0} , 50000 : {1}",i,j);
+                    }
+                }
+            }
+            Console.ReadKey();
